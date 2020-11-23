3e0745bdcb3d122b7143582dd2fd3b0a655dc8c2
ashfaque ali mohd.mushtaque ali <54464032+aliashfak178@users.noreply.github.com>
2020-11-23 17:22:06 +0530

    init

---
+++ Utility/DBConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Utility
+{
+    public class DBConfiguration
+    {
+        private static string connectionString;
+
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings
+                    ["ExpDB"].ConnectionString;
+            }
+        }
+    }
+}
